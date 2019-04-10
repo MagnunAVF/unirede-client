61034f82830fff759d0da7ff2d144fb39f9dbad6
Magnun A V F <magnunavf@gmail.com>
2019-04-10 19:32:12 -0300

    feat: Improve Footer component

--- src/components/structure/Footer.jsx
+++ src/components/structure/Footer.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCodeBranch } from '@fortawesome/free-solid-svg-icons'
 
 const Footer = () => (
   <footer>
-    <h1>Footer</h1>
+    <div className="container d-flex justify-content-center">
+      <span>desenvolvido por MagnunAVF </span>
+      <a href="https://github.com/magnunAVF">
+        <FontAwesomeIcon icon={faCodeBranch} color="white" />
+      </a>
+    </div>
   </footer>
 )
 
